56049fe569335ec59ec1e1d7e772165e88573c22
Randhir <rps85071@gmail.com>
2021-08-02 20:57:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import StateWise from './Component/statewiseData/StateWise';
+import Footer from './Component/statewiseData/Footer';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <StateWise />
+      <Footer />
     </div>
   );
 }
---
+++ src/Component/statewiseData/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './statewise.css'
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+
+                <p>By: Randhir Pratap Singh</p>
+            </footer>
+        </div>
+    )
+}
